e135c2f813b233a84cdabec62a5db6d5d9465e11
Hanler <hmelnitzkij.danil85@gmail.com>
2024-07-28 12:07:37 +0200

    worker: Add a basic implementation of the NNWorker class

---
+++ backend/nn_worker/NNWorker.py
@@ -0,0 +1,18 @@
+from celery import Celery
+import time
+
+
+class NNWorker(object):
+    """
+    A class used to represent a neural network worker that processes articles
+    """
+    def __init__(self):     
+        print("NNWorker was inited")
+
+    def start(self, article):
+        """
+        Run the NN worker
+        """
+        print(f"Worker gets the task: {article}")
+        time.sleep(3)
+        print(f"End task {article}")
