54afa423aab0097c32be656e1a124e5c134c75b1
Hanler <hmelnitzkij.danil85@gmail.com>
2024-07-31 17:09:33 +0200

    main: Handle all workers with Celery and queues

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,17 @@
-import threading
-from scrapers.ArticleScraper import ArticleScraper
-from scrapers.LinkScraper import LinkScraper
+from task_queue.tasks import link_scraper_task
+from config.settings import SITES_STACK
 
-
-def producer_task(producer, tasks):
-    for task in tasks:
-        producer.send_task(task)
-
-def main_multithreading():
-    # Create instances of ArticleScraper (producers)
-    producers = [ArticleScraper() for _ in range(5)]
-
-    # List of tasks to be processed
-    tasks = [f"Article {i}" for i in range(10)]
-
-    # Create and start threads for each producer
-    threads = []
-    for producer in producers:
-        thread = threading.Thread(target=producer_task, args=(producer, tasks))
-        threads.append(thread)
-        thread.start()
-
-    # Wait for all threads to complete
-    for thread in threads:
-        thread.join()
+def launch_link_scrapers(links):
+    """
+    Launch the LinkScraper instances
+    with links from the pre-defined stack
+    """
+    for link in links:
+        link_scraper_task.apply_async(args=[link], queue='link_scraper')
 
 def main():
-    producer = LinkScraper(link="https://www.bbc.com/")
-    # producer = ArticleScraper(link="https://www.bbc.com/")
+    # producer = LinkScraper(link="https://www.bbc.com/")
+    launch_link_scrapers(SITES_STACK)
 
 if __name__ == '__main__':
     main()
