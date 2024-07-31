d71adbb92ba0661bd7b22b5ebf693ca472297311
Hanler <hmelnitzkij.danil85@gmail.com>
2024-07-31 17:13:34 +0200

    LinkScraper: Advance DB answer handling | Push new links to ArticleScrapers' queue

--- backend/scrapers/LinkScraper.py
+++ backend/scrapers/LinkScraper.py
@@ -2,7 +2,8 @@ import time
 from urllib.parse import urljoin, urlparse
 from scrapers.Scraper import Scraper
 from db.database import database_instance
-
+from firebase_admin.exceptions import FirebaseError
+from task_queue.tasks import article_scraper_task
 
 class LinkScraper(Scraper):
     """
@@ -11,8 +12,11 @@ class LinkScraper(Scraper):
     """
     def __init__(self, link):       
         super().__init__(link)
+        self.new_links = []
         self.links = self.get_links_from_html()
-        self.send_links_to_db()
+
+        if self.send_links_to_db():
+            self.push_links_to_queue()
 
     def get_links_from_html(self):
         """
@@ -74,15 +78,46 @@ class LinkScraper(Scraper):
 
                 counter += 1
 
+                # Create an initial object
                 data = {
                     "id": doc_ref.id,
                     "url": link,
-                    "tags": [],
-                    "date": formatted_date,
-                    "domain": self.domain,
                 }
+
+                self.new_links.append(data)
+
+                # Add other values
+                data['tags'] = []
+                data['date'] = formatted_date
+                data['domain'] = self.domain
 
                 batch.set(doc_ref, data)
 
-        batch.commit()
-        print(f"Were pushed links to DB: {counter}")
+        # Skip a commit to DB if the batch is empty
+        if not len(self.new_links):
+            return True
+        
+        try:
+            commit_result = batch.commit()
+
+            # Check if the commit result is not empty, indicating success
+            if commit_result:
+                print("Batch commit successful")
+                print(f"Were pushed links to DB: {counter}")
+                return True
+            else:
+                print("Batch commit failed, no documents committed")
+        except FirebaseError as e:
+            print(f"An error occurred while committing the batch: {e}")
+        except Exception as e:
+            print(f"An unexpected error has occurred: {e}")
+
+        return False
+
+    def push_links_to_queue(self):
+        """
+        Push new links to Link-Article Scrapers' queue 
+        """
+        print("Starting pushing links to queue")
+        for link in self.new_links:
+            article_scraper_task.apply_async(args=[link['id'], link['url']], queue='article_scraper')
