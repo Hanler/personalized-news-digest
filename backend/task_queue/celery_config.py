5a299fbfba6da6f602707ea3a3fc73359c1cb89f
Hanler <hmelnitzkij.danil85@gmail.com>
2024-07-31 17:10:53 +0200

    celery: Define Celery's config

--- backend/task_queue/celery_config.py
+++ backend/task_queue/celery_config.py
@@ -2,4 +2,28 @@ from celery import Celery
 from config.settings import BROKER_URL, RESULT_BACKEND
 
 
-app = Celery('tasks', broker=BROKER_URL, backend=RESULT_BACKEND)
+class CeleryConfig:
+    broker_url = BROKER_URL
+    result_backend = RESULT_BACKEND
+    task_queues = {
+        'link_scraper': {
+            'exchange': 'link_scraper',
+            'routing_key': 'link_scraper'
+        },
+        'article_scraper': {
+            'exchange': 'article_scraper',
+            'routing_key': 'article_scraper'
+        },
+        'nn_worker': {
+            'exchange': 'nn_worker',
+            'routing_key': 'nn_worker'
+        }
+    }
+    task_routes = {
+        'tasks.link_scraper_task': {'queue': 'link_scraper'},
+        'tasks.article_scraper_task': {'queue': 'article_scraper'},
+        'tasks.nn_worker_task': {'queue': 'nn_worker'},
+    }
+
+app = Celery('tasks')
+app.config_from_object(CeleryConfig)
