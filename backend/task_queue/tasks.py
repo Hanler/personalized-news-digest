0531aff757fad857eed796729d15a8c5c1fc22d1
Hanler <hmelnitzkij.danil85@gmail.com>
2024-07-31 17:10:27 +0200

    celery: Define Celery's tasks

--- backend/task_queue/tasks.py
+++ backend/task_queue/tasks.py
@@ -1,13 +1,28 @@
+import os, sys
 from task_queue.celery_config import app
 from nn_worker.NNWorker import NNWorker
 
+# Add backend directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # This will store the NNWorker instance
-worker_instance = None
+nn_worker_instance = None
 
 @app.task
-def process_task(task):
-    global worker_instance
-    if worker_instance is None:
-        worker_instance = NNWorker()
-    worker_instance.start(task)
+def link_scraper_task(link):
+    from scrapers.LinkScraper import LinkScraper
+    print(f"LinkScraper was spawned with {link}")
+    link_scraper_instance = LinkScraper(link)
+
+@app.task
+def article_scraper_task(id, link):
+    from scrapers.ArticleScraper import ArticleScraper 
+    print(f"ArticleScraper was spawned with {id} -- {link}")
+    article_scraper_instance = ArticleScraper(id, link)
+
+@app.task
+def nn_worker_task(id, text):
+    global nn_worker_instance
+    if nn_worker_instance is None:
+        nn_worker_instance = NNWorker()
+    nn_worker_instance.start(id, text)
